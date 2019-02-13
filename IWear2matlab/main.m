0c348259629cf84435748544d5ee510867934103
Claudia Latella <claudia.latella.86@gmail.com>
2019-02-13 16:57:58 +0100

    [WIP] Added initial code to parse the IWear dumped files into Matlab

---
+++ IWear2matlab/main.m
@@ -0,0 +1,31 @@
+
+%% Preliminaries
+clc; close all; clear all;
+
+addpath(genpath('src'));
+addpath(genpath('data'));
+
+addpath(genpath('../src'));
+
+opts.sensorsPos = true;
+
+disp(strcat('[Start] wearData extraction ...'));
+
+%% Extract data from IWearRemapper dumped file
+extractWearableDataFromIWear;
+
+%% Compute sensor position
+if opts.sensorsPos
+    % Optional: computation of the position of the sensors w.r.t. the frame
+    %           of the attached link
+    wearData = computeSuitSensorPosition(wearData);
+end
+
+%% Save struct
+if ~exist(fullfile(pwd,'processed'),'dir')
+    mkdir (fullfile(pwd,'processed'));
+end
+save(fullfile(pwd,'processed/wearData.mat'),'wearData');
+
+disp(strcat('[End] wearData extraction'));
+
